93377abea7f082148b8ed1319e8a873132d88b64
vishal493gupta <vishalgupta493@gmail.com>
2024-02-13 01:12:56 +0530

    A new Python Program is creating that is password generator.

---
+++ Password_Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the PyPassword Generator")
+letters = int(input("How many letters would you like in your password?"))
+symbols = int(input("How many symbols would you like?"))
+number = int(input("How many numbers would you like?"))
+print("Here is your password: ", password)

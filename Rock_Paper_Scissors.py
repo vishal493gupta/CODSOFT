62b6f1bc0749d0e4dd77559f1b3afde497f83a6b
vishal493gupta <vishalgupta493@gmail.com>
2024-02-24 21:47:47 +0530

    modify some logic of code

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -11,23 +11,34 @@ Rock="✊"
 Paper="✋"
 Scissors="✌️"
 
-map=['Rock', 'Paper',' Scissors']
+user_score = 0
+computer_score = 0
 
-comp_choose=random.choice(map).lower()
+map=['Rock', 'Paper','Scissors']
 
-you_choose=input("What would you like to choose ? Type Rock, Paper or Scissors : ").lower()
+while True:
+    comp_choose=random.choice(map).lower()
+    you_choose=input("What would you like to choose ? Type Rock, Paper or Scissors : ").capitalize()
 
-if comp_choose==you_choose:
-	print(f"you choose {you_choose} and computer choose {comp_choose} , both are same thats why it is tie(draw).")
-	
-elif (you_choose==Rock )and (comp_choose==Scissors):
-	print(f"you choose {you_choose} and computer choose {comp_choose}, Congrats 😇, you have won..")
-	
-elif (you_choose==Paper )and (comp_choose==Rock):
-	print(f"you choose {you_choose} and computer choose {comp_choose}, Congrats 😇, you have won..")
-	
-elif (you_choose==Scissors )and (comp_choose==Paper):
-	print(f"you choose {you_choose} and computer choose {comp_choose}, Congrats 😇, you have won..")
-	
-else:
-	print(f"you choose {you_choose} and computer choose {comp_choose}, Oops 😢, you lost the game.")
+    if comp_choose==you_choose.lower():
+        print(f"you choose {you_choose} and computer choose {comp_choose} , both are same thats why it is tie(draw).")
+        
+    elif (you_choose=="Rock" )and (comp_choose=="scissors"):
+        print(f"you choose {you_choose} and computer choose {comp_choose}, Congrats 😇, you have won..")
+        user_score += 1
+        
+    elif (you_choose=="Paper" )and (comp_choose=="rock"):
+        print(f"you choose {you_choose} and computer choose {comp_choose}, Congrats 😇, you have won..")
+        user_score += 1
+        
+    elif (you_choose=="Scissors" )and (comp_choose=="paper"):
+        print(f"you choose {you_choose} and computer choose {comp_choose}, Congrats 😇, you have won..")
+        user_score += 1
+        
+    else:
+        print(f"you choose {you_choose} and computer choose {comp_choose}, Oops 😢, You lose! Try again..")
+        computer_score += 1
+    
+    play_again = input("Do you want to play again? (y/n): ")
+    if play_again.lower() != "y":
+            break

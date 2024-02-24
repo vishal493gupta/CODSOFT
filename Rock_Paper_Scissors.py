41178931dd5e2a936265e190ad2562b69b6cfbca
vishal493gupta <vishalgupta493@gmail.com>
2024-02-24 20:45:25 +0530

    Write the logic part of rock game.

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -4,3 +4,30 @@
 1. Rock wins against Scissors.
 2. Scissors win against paper.
 3. Papers wins against rock.'''
+
+import random
+
+Rock="✊"
+Paper="✋"
+Scissors="✌️"
+
+map=['Rock', 'Paper',' Scissors']
+
+comp_choose=random.choice(map).lower()
+
+you_choose=input("What would you like to choose ? Type Rock, Paper or Scissors : ").lower()
+
+if comp_choose==you_choose:
+	print(f"you choose {you_choose} and computer choose {comp_choose} , both are same thats why it is tie(draw).")
+	
+elif (you_choose==Rock )and (comp_choose==Scissors):
+	print(f"you choose {you_choose} and computer choose {comp_choose}, Congrats 😇, you have won..")
+	
+elif (you_choose==Paper )and (comp_choose==Rock):
+	print(f"you choose {you_choose} and computer choose {comp_choose}, Congrats 😇, you have won..")
+	
+elif (you_choose==Scissors )and (comp_choose==Paper):
+	print(f"you choose {you_choose} and computer choose {comp_choose}, Congrats 😇, you have won..")
+	
+else:
+	print(f"you choose {you_choose} and computer choose {comp_choose}, Oops 😢, you lost the game.")
